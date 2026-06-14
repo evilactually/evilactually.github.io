e5ef24cf9adccd9264a122e6955105893b35c4df
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2026-06-14 10:24:09 -0600

    Draft

--- vynil_record_player_part1/src.php
+++ vynil_record_player_part1/src.php
@@ -1,7 +1,5 @@
 <?php genheader("Vinyl Record Player - Constant Speed Control", "June 14, 2026");?>
 
-<?php h("INTRODUCTION");?>
-
 <p>
     In this series of articles I will be building a vinyl record player mostly from scratch. I will be using an off-the-shelf motor and a headshell, but otherwise build all audio and power amplifiers myself. 
     I think this is a great way to demonstrate amplifier design and implementation.
@@ -13,12 +11,12 @@
 </p>
 
 <p>
-    We can build a relatively simple speed controller using a DC motor with hall sensors and somed feedback loop.
+    We can build a relatively simple speed controller using a DC motor with hall sensors and some feedback loop. Take hall sensor signal, pass it through low-pass filter. Take the difference between the filtered feedback signal and a set point, invert it and amplify it. Finally, use amplified error as control signal to drive the motor. This is a standard proportional control loop that can be implemented with discreet components.
 </p>
 
 
 <?php h("LINKS");?>
 <p>
-    Add reference links here.
+    <a href="https://www.adafruit.com/product/4416">Geared DC Motor with Magnetic Encoder Outputs - 7 VDC 1:20 Ratio</a>.
 </p>
 
